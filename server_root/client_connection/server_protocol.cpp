c9ef340b4afea3f7736fb16b1a1d3477dab6cb1f
lara-scazzola1 <lscazzola@fi.uba.ar>
2023-06-12 16:29:39 -0300

    cambio protocolo servidor

--- server_root/client_connection/server_protocol.cpp
+++ server_root/client_connection/server_protocol.cpp
@@ -4,74 +4,59 @@
 
 #include <iostream>
 
-/*ServerProtocol::ServerProtocol(const std::string& servname) :
-    servname(servname){}*/
-
 ServerProtocol::ServerProtocol(void) {}
 
-void ServerProtocol::notify_create(uint32_t code, Socket &peer, bool &was_closed) {
-    uint32_t buf_code[1];
-    buf_code[0] = htonl(code);
-    peer.sendall(buf_code, 4, &was_closed);
+int ServerProtocol::receiveTypeCommand(bool &wasClosed, Socket &peer){
+    uint8_t type;
+    peer.recvsome(&type, 1, &wasClosed);
+    return type;
 }
 
-void ServerProtocol::notify_join(uint8_t param, Socket &peer, bool &was_closed) {
-    uint8_t id[1];
-    id[0] = param;
-    peer.sendsome(id, 1, &was_closed);
-}
+std::string ServerProtocol::receiveCreate(bool &wasClosed, Socket &peer){
+    uint8_t scenario_len;
+    peer.recvall(&scenario_len, 1, &wasClosed);
 
-void ServerProtocol::send_update_move(uint32_t posx, uint32_t posy, bool &was_closed, Socket &peer) {
-    uint32_t buf_posx[1];
-    buf_posx[0] = posx;
-    buf_posx[0] = htonl((uint32_t)buf_posx[0]);
-    peer.sendall(buf_posx, 4, &was_closed);
-    if (was_closed)
-        return;
-
-    uint32_t buf_posy[1];
-    buf_posy[0] = posy;
-    buf_posy[0] = htonl((uint32_t)buf_posy[0]);
-    peer.sendall(buf_posy, 4, &was_closed);
-}
-
-int ServerProtocol::receive_type_of_command(bool &was_closed, Socket &peer) {
-    uint8_t buf[1];
-    peer.recvsome(buf, 1, &was_closed);
-
-    return (int)buf[0];
-}
-
-std::string ServerProtocol::receive_create(bool &was_closed, Socket &peer) {
-    uint16_t buf_scenario_len[1];
-    peer.recvall(buf_scenario_len, 2, &was_closed);
-    uint16_t scenario_len = ntohs(buf_scenario_len[0]);
-
-    char buf_scenario_name[100];  // despues ver cual es el max, creo que esta def
-    peer.recvall(buf_scenario_name, scenario_len, &was_closed);
+    char buf_scenario_name[500];
+    peer.recvall(buf_scenario_name, scenario_len, &wasClosed);
     buf_scenario_name[scenario_len] = '\0';
 
     return buf_scenario_name;
 }
 
-uint32_t ServerProtocol::receive_join(bool &was_closed, Socket &peer) {
-    uint32_t buf_code[1];
-    peer.recvall(buf_code, 4, &was_closed);
-    uint32_t code = ntohl(buf_code[0]);
-
+uint8_t ServerProtocol::receiveJoin(bool &wasClosed, Socket &peer){
+    uint8_t code;
+    peer.recvall(&code, 1, &wasClosed);
     return code;
 }
 
-std::vector<int> ServerProtocol::receive_start_move(bool &was_closed, Socket &peer) {
+std::vector<int> ServerProtocol::receiveStartMove(bool& wasClosed, Socket &peer){
     std::vector<int> vector;
 
-    uint32_t buf_difx[1];
-    peer.recvall(buf_difx, 4, &was_closed);
-    vector.push_back(ntohl(buf_difx[0]));
+    uint8_t typeMove;
+    peer.recvall(&typeMove, 1, &wasClosed);
+    vector.push_back(typeMove);
 
-    uint32_t buf_dify[1];
-    peer.recvall(buf_dify, 4, &was_closed);
-    vector.push_back(ntohl(buf_dify[0]));
+    int8_t x;
+    peer.recvall(&x, 1, &wasClosed);
+    vector.push_back(x);
+
+    int8_t y;
+    peer.recvall(&y, 1, &wasClosed);
+    vector.push_back(y);
+
+    return vector;
+}
+
+std::vector<int> ServerProtocol::receiveEndMove(bool& wasClosed, Socket &peer){
+    std::vector<int> vector;
+
+    int8_t x;
+    peer.recvall(&x, 1, &wasClosed);
+    vector.push_back(x);
+
+    int8_t y;
+    peer.recvall(&y, 1, &wasClosed);
+    vector.push_back(y);
 
     return vector;
 }
