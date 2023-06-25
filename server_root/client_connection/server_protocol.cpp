e0f784b6a80952f416d31e9ce6b1f7826a043916
Regazzoli Ignacio <ignacioregazzoli@gmail.com>
2023-06-24 21:49:37 -0300

    Fix error with coordinates

--- server_root/client_connection/server_protocol.cpp
+++ server_root/client_connection/server_protocol.cpp
@@ -141,39 +141,37 @@ std::shared_ptr<std::vector<uint8_t>> ServerProtocol::encodeServerMessage(std::s
         encodedMsg->push_back(reinterpret_cast<uint8_t *>(&encodedHealth)[1]);
     }
 
-    std::cout << "Encoded Message: ";
+    // std::cout << "Encoded Message: ";
 
-    int byteCount = 0;
-    int entityByteCount = -3;  // Exclude the first 3 bytes for counting bytes in entities
-    for (auto byte : *encodedMsg) {
-        byteCount++;
-        entityByteCount++;
+    // int byteCount = 0;
+    // int entityByteCount = -3;  // Exclude the first 3 bytes for counting bytes in entities
+    // for (auto byte : *encodedMsg) {
+    //     byteCount++;
+    //     entityByteCount++;
 
-        // Add separator after the first 3 bytes
-        if (byteCount == 4) {
-            std::cout << "| ";
-        }
+    //     // Add separator after the first 3 bytes
+    //     if (byteCount == 4) {
+    //         std::cout << "| ";
+    //     }
 
-        // Add separator between entities (after every 13 bytes for zombies, excluding the first 3 bytes)
-        if (byteCount > 4 && entityByteCount % 13 == 0) {
-            std::cout << "| ";
-        }
+    //     // Add separator between entities (after every 13 bytes for zombies, excluding the first 3 bytes)
+    //     if (byteCount > 4 && entityByteCount % 13 == 0) {
+    //         std::cout << "| ";
+    //     }
 
-        // Color the 4th byte of zombies red and the 3rd byte blue
-        if (byteCount > 4) {
-            if (entityByteCount % 13 == 3) {
-                std::cout << "\033[1;34m";
-            } else if (entityByteCount % 13 == 4) {
-                std::cout << "\033[1;31m";
-            }
-        }
+    //     // Color the 4th byte of zombies red and the 3rd byte blue
+    //     if (byteCount > 4) {
+    //         if (entityByteCount % 13 == 3) {
+    //             std::cout << blueText;
+    //         } else if (entityByteCount % 13 == 4) {
+    //             std::cout << redText;
+    //         }
+    //     }
 
-        // Print byte in decimal
-        std::cout << static_cast<int>(byte) << " ";
-    }
-
-    std::cout << "\033[0m";
-    std::cout << std::endl;
+    //     // Print byte in decimal
+    //     std::cout << static_cast<int>(byte) << " ";
+    // }
+    // std::cout << std::endl;
 
     return encodedMsg;
 }
